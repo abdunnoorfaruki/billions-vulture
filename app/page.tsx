a921f993042bc9fadf85ccd0f86d8817f023daea
Abdun Noor Faruki Biswas <86634583+abdunnoorfaruki@users.noreply.github.com>
2026-03-19 10:40:07 +0530

    Initial build from Vibe Coder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Hero3DLanding from "./hero-3d-landing";
+import { Hero3DLanding } from "./hero-3d-landing";
 
-export default function Home() {
+export default function Page() {
   return <Hero3DLanding />;
 }
